d3068d0afdaa377d1191c40a14c4992b2f31a646
LuigiTCastro <luigicastro29@gmail.com>
2023-05-18 22:49:22 -0300

    Finally!

--- services/imagesUploadCosmic.ts
+++ services/imagesUploadCosmic.ts
@@ -1,16 +1,18 @@
 import multer from 'multer';
 import cosmicjs from 'cosmicjs';
 
-const { WRITE_KEY_AVATARS,
+const { 
+    CHAVE_GRAVACAO_AVATARES,
+    BUCKET_AVATARES,
     WRITE_KEY_PUBLICATIONS,
-    BUCKET_AVATARS,
-    BUCKET_PUBLICATIONS } = process.env;
+    BUCKET_PUBLICATIONS 
+} = process.env;
 
 const Cosmic = cosmicjs();
 
 const avatarsBucket = Cosmic.bucket({
-    slug: BUCKET_AVATARS,
-    write_key: WRITE_KEY_AVATARS
+    slug: BUCKET_AVATARES,
+    write_key: CHAVE_GRAVACAO_AVATARES
 });
 
 const publicationsBucket = Cosmic.bucket({
@@ -19,35 +21,34 @@ const publicationsBucket = Cosmic.bucket({
 });
 
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const upload = multer({ storage : storage });
 
-const imagesUploadCosmic = async (req: any) => { // This function gets the raw data contained in the properties of FILE and transforms in a Cosmic OBJECT including informations as MEDIA and URL.
-
+const imagesUploadCosmic = async(req : any) => {
+    
+    // console.log(WRITE_KEY_AVATARS);
     console.log(req.body)
     console.log(req.file.originalname)
-
-    if (req?.file?.originalname) {
-
-        if (!req.file.originalname.includes('.png') &&
-            !req.file.originalname.includes('.jpg') &&
-            !req.file.originalname.includes('.jpeg')) {
-
-            throw new Error('Image extension not valid.');
-        }
-
+    
+    if(req?.file?.originalname) {
         const media_object = {
-            originalname: req.file.originalname,
-            buffer: req.file.buffer
-        };
-        
-        if (req.url && req.url.includes('publication')) {
-            return publicationsBucket.addMedia({ media: media_object })
+            originalname : req.file.originalname,
+            buffer : req.file.buffer
+        }
+        if(req.url && req.url.includes('publication')) {
+            return await publicationsBucket.addMedia({ media : media_object })
         }
         else {
-            return avatarsBucket.addMedia({ media: media_object })
+            return await avatarsBucket.addMedia({ media : media_object })
         }
     }
 }
 
 export { upload, imagesUploadCosmic };
 
+
+            // if (!req.file.originalname.includes('.png') &&
+            //     !req.file.originalname.includes('.jpg') &&
+            //     !req.file.originalname.includes('.jpeg')) {
+    
+            //     throw new Error('Image extension not valid.');
+            // }
