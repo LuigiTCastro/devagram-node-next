2321bfc18f5d7c740045922fa6e8590d330e1414
LuigiTCastro <luigicastro29@gmail.com>
2023-05-21 21:56:44 -0300

    Follow endpoint updated

--- models/FollowerModel.ts
+++ models/FollowerModel.ts
@@ -6,7 +6,7 @@ const FollowerSchema = new Schema({
     userId : {type : String, required : true },
 
     // who is followed
-    followedUser : {type : String, required : true },
+    followedUserId : {type : String, required : true },
 });
 
 export const FollowerModel = (mongoose.models.followers || mongoose.model('followers', FollowerSchema));
--- pages/api/follow.ts
+++ pages/api/follow.ts
@@ -3,6 +3,7 @@ import { DefaultResponseMsg } from "../../types/DefaultResponseMsg";
 import { JWTTokenValidate } from "../../middlewares/JWTTokenValidate";
 import { MongoDBconnect } from "../../middlewares/MongoDBconnect";
 import { UserModel } from "../../models/UserModel";
+import { FollowerModel } from "../../models/FollowerModel";
 
 const endpointFollow = async ( req : NextApiRequest, res : NextApiResponse<DefaultResponseMsg>) => {
 
@@ -24,6 +25,43 @@ const endpointFollow = async ( req : NextApiRequest, res : NextApiResponse<Defau
 
             if(!userToBeFollowed) {
                 return res.status(400).json({ error: 'User to be followed not found.' });
+            }
+
+            const userAlreadyFollowedByMe = await FollowerModel.find( { userId : loggedUser._id, id : userToBeFollowed._id }); 
+            // Makes a filter with two parameters: 1) gets who has the id of logged user. 2) gets who has the id of user to be followed.
+            // The idea is to check if any relationship already exists between the users.
+
+            if(userAlreadyFollowedByMe && userAlreadyFollowedByMe.length > 0) {
+                // Delete the user and Update the following/followers.
+                userAlreadyFollowedByMe.forEach(async(e : any) => await FollowerModel.findByIdAndDelete({_id : e._id}));
+                
+                loggedUser.followings--;
+                await UserModel.findByIdAndUpdate({ _id : loggedUser._id }, loggedUser);
+
+                userToBeFollowed.followers--;
+                await UserModel.findByIdAndUpdate({ _id : userToBeFollowed._id }, userToBeFollowed);
+                
+                return res.status(200).json({ msg: 'User unfollowed successfully!' });
+            }
+            
+            else {
+                // if i do not follow the user: to create the relationship.
+
+                const follower = {
+                    userId : loggedUser._id,
+                    followedUserId : userToBeFollowed._id
+                }
+                await FollowerModel.create(follower);
+
+                // Updates the FOLLOWING of the logged user.
+                loggedUser.followings++;
+                await UserModel.findByIdAndUpdate({ _id : loggedUser._id }, loggedUser);
+
+                // Updates the followed of the user to be followed.
+                userToBeFollowed.followed++;
+                await UserModel.findByIdAndUpdate({ _id : userToBeFollowed._id }, userToBeFollowed);
+                
+                return res.status(200).json({ msg: 'User followed successfully!' });
             }
         }
 
