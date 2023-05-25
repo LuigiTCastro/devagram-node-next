581e5770e09021754f961f3510466d5458a3e308
LuigiTCastro <luigicastro29@gmail.com>
2023-05-24 23:05:13 -0300

    Cleaning up

--- middlewares/CORSpolicy.ts
+++ middlewares/CORSpolicy.ts
@@ -2,19 +2,15 @@ import type { NextApiRequest, NextApiResponse, NextApiHandler } from "next";
 import { DefaultResponseMsg } from "../types/DefaultResponseMsg";
 import NextCors from 'nextjs-cors';
 
-// Como se trata de um middleware, é necessário receber o handler primeiro.
-// CORS: Cross-Origin
 export const CORSpolicy = (handler: NextApiHandler) =>
     async (req: NextApiRequest, res: NextApiResponse<DefaultResponseMsg>) => {
 
         try {
             
             await NextCors(req, res, {
-                // origin: 'localhost:3000, devagram.com.br'
-                origin: '*', // Astherisc to allow public access.
+                origin: '*',
                 methods: ['GET, POST, PUT'],
-                // header: [...]
-                optionsSuccessStatus : 200 // Old browsers have problems when returns 204.
+                optionsSuccessStatus : 200
             })
 
             return handler(req, res);
