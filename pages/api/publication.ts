f6845e62e7d5a111d9358b913dc96a51ce0134f1
LuigiTCastro <luigicastro29@gmail.com>
2023-05-24 22:30:27 -0300

    Publication endpoint adjusted

--- pages/api/publication.ts
+++ pages/api/publication.ts
@@ -1,94 +1,17 @@
 // PRIVATE ROUTE. SO ITS NECESSARY TO BE LOGGED TO MAKE A PUBLICATION.
 
-// import { imagesUploadCosmic, upload } from "../../services/imagesUploadCosmic";
-// import { DefaultResponseMsg } from "../../types/DefaultResponseMsg";
-// import { MongoDBconnect } from "../../middlewares/MongoDBconnect";
-// import { JWTTokenValidate } from "../../middlewares/JWTTokenValidate";
-// import { PublicationModel } from '../../models/PublicationModel';
-// import { UserModel } from '../../models/UserModel';
-// import type { NextApiResponse } from "next";
-// import nc from 'next-connect';
-
-
-// const handler = nc()
-//     .use(upload.single('file')) // upload do multer. 'file': name of the field to pass as key in the form-data (postman) 
-//     .post(async ( req : any, res : NextApiResponse<DefaultResponseMsg> ) => {
-       
-//         try {
-//             console.log('endpoint publish', req.body);
-
-//             const { userId } = req.query;
-//             const user = await UserModel.findById(userId);
-
-//             if(!user) {
-//                 return res.status(400).json({ error: 'User not found.' });
-//             }
-
-//             if(!req || !req.body) {
-//                 return res.status(400).json({ error: 'Input parameters not informed.' });
-//             }
-
-//             const { description } = req?.body;
-
-//             if(!description || description.length < 2) {
-//                 return res.status(400).json({ error: 'Description is not valid.' });
-//             }
-            
-//             if(!req.file || !req.file.originalname) {
-//                 return res.status(400).json({ error: 'Image is required.' });
-//             }
-            
-//             const image = await imagesUploadCosmic(req);
-//             const publication = {
-//                 userId : user._id,
-//                 description,
-//                 image : image.media.url,
-//                 date : new Date()
-//             }
-
-//             user.publications++;
-//             await UserModel.findByIdAndUpdate({_id : user._id}, user);
-
-//             await PublicationModel.create(publication);
-
-//             return res.status(200).json({ msg: 'Publication created successfully.' });
-//         }
-
-//         catch(e) {
-//             console.log(e);
-//             return res.status(400).json({ error: 'Error when trying to publish.'});
-//         }
-//     });
-
-// export const config = {
-//     api : {
-//         bodyparser : false
-//     }
-// }
-
-// export default JWTTokenValidate(MongoDBconnect(handler));
-
-
-
-
-
-// --------------------------------------------
-
-
-
-
-import type {NextApiResponse} from 'next';
-import { DefaultResponseMsg } from "../../types/DefaultResponseMsg";
-import nc from 'next-connect';
 import {upload, imagesUploadCosmic} from "../../services/imagesUploadCosmic";
+import { DefaultResponseMsg } from "../../types/DefaultResponseMsg";
 import { MongoDBconnect } from "../../middlewares/MongoDBconnect";
 import { JWTTokenValidate } from "../../middlewares/JWTTokenValidate";
 import { PublicationModel } from '../../models/PublicationModel';
-import { UserModel } from '../../models/UserModel';
 import { CORSpolicy } from "../../middlewares/CORSpolicy";
+import { UserModel } from '../../models/UserModel';
+import type {NextApiResponse} from 'next';
+import nc from 'next-connect';
 
 const handler = nc()
-    .use(upload.single('file'))
+    .use(upload.single('file')) // upload do multer. 'file': name of the field to pass as key in the form-data (postman).
     .post(async (req : any, res : NextApiResponse<DefaultResponseMsg>) => {
         
         try{
@@ -97,25 +20,26 @@ const handler = nc()
             const user = await UserModel.findById(userId);
             
             if(!user){
-                return res.status(400).json({error : 'user nao encontrado'});
+                return res.status(400).json({ error: 'User not found.' });
             }
 
             if(!req || !req.body){
-                return res.status(400).json({error : 'Parametros de entrada nao informados'});
+                return res.status(400).json({ error: 'Input parameters not informed.' });
             }
             
             const {description} = req?.body;
 
             if(!description || description.length < 2){
-                return res.status(400).json({error : 'description nao e valida'});
+                return res.status(400).json({ error: 'Description is not valid.' });
             }
     
             if(!req.file || !req.file.originalname){
-                return res.status(400).json({error : 'Imagem e obrigatoria'});
+                return res.status(400).json({ error: 'Image is required.' });
             }
 
             const image = await imagesUploadCosmic(req);
-            const publicacao = {
+            
+            const publication = {
                 userId : user._id,
                 description,
                 image : image.media.url,
@@ -124,12 +48,14 @@ const handler = nc()
 
             user.publications++;
             await UserModel.findByIdAndUpdate({_id : user._id}, user);
-
-            await PublicationModel.create(publicacao);
-            return res.status(200).json({msg : 'Publicacao criada com sucesso'});
-        }catch(e){
+            await PublicationModel.create(publication);
+            
+            return res.status(200).json({ msg: 'Publication created successfully.' });
+        }
+        
+        catch(e){
             console.log(e);
-            return res.status(400).json({error : 'Erro ao cadastrar publicacao'});
+            return res.status(400).json({ error: 'Error when trying to publish.'});
         }
 });
 
